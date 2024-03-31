5b27fefc15a520a7a6bf78ae03b8c8054954f0f6
JustFélix_ <85786075+LeBazarDeBryan@users.noreply.github.com>
2024-03-31 23:29:22 +0200

    Update
    France_4.py

--- Stream/Python/France_4.py
+++ Stream/Python/France_4.py
@@ -5,8 +5,9 @@ import requests
 print('#EXTM3U')
 print('#EXT-X-VERSION:5')
 print('#EXT-X-STREAM-INF:BANDWIDTH=3032655,AVERAGE-BANDWIDTH=2756959,CODECS="avc1.64001f,mp4a.40.2",RESOLUTION=1280x720,FRAME-RATE=25.000,AUDIO="audio-AACL-96",SUBTITLES="text"')
+
 s = requests.Session()
-response = s.get(f'https://hdfauth.ftven.fr/esi/TA?url=https://simulcast-p.ftven.fr/simulcast/France_4/hls_fr4/index.m3u8')
+response = s.get(f'https://hdfauth.ftven.fr/esi/TA?url=https://simulcast-b.ftven.fr/simulcast/France_4/hls_fr4/index.m3u8')
 
 string = response.text
 new_string = string.replace("index", "France_4-avc1_2500000=10001")
