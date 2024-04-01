921e00f14fffdb38be8aa5fdec52f75f2f4b5add
JustFélix_ <85786075+LeBazarDeBryan@users.noreply.github.com>
2024-04-01 12:55:29 +0200

    Update
    RMC_Story.py

--- Stream/Python/RMC_Story.py
+++ Stream/Python/RMC_Story.py
@@ -1,55 +1,37 @@
+#!/usr/bin/python3
+
 import requests
-import os
-import sys
 
-proxies = {}
-if len(sys.argv) == 2:
-    proxies = {
-                'http' : sys.argv[1],
-                'https' : sys.argv[1]
-              }
-
-na = 'https://raw.githubusercontent.com/LeBazarDeBryan/XTVZ_/main/Images/Offline.mp4'
-def grab(line):
-    try:
-        _id = line.split('/')[4]
-        response = s.get(f'https://www.dailymotion.com/player/metadata/video/{_id}', proxies=proxies).json()['qualities']['auto'][0]['url']
-        m3u = s.get(response, proxies=proxies).text
-        m3u = m3u.strip().split('\n')[1:]
-        d = {}
-        cnd = True
-        for item in m3u:
-            if cnd:
-                resolution = item.strip().split(',')[2].split('=')[1]
-                if resolution not in d:
-                    d[resolution] = []
-            else:
-                d[resolution]= item
-            cnd = not cnd
-        m3u = d[max(d, key=int)]    
-    except Exception as e:
-        m3u = na
-    finally:
-        print(m3u)
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0",
+    "Referer": "http://www.callofliberty.fr/"
+}
 
 print('#EXTM3U')
-print('#EXT-X-VERSION:3')
-print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000')
+print('#EXT-X-STREAM-INF:RESOLUTION=848x477,FRAME-RATE=50.000000,BANDWIDTH=1667072,CODECS="avc1.64001f,mp4a.40.2",NAME="480@60"')
+
+master_url = "http://s2.callofliberty.fr/direct/RMCSTORY/master.m3u8"
 s = requests.Session()
-with open('Stream/Info/RMC_Story.txt') as f:
-    for line in f:
-        line = line.strip()
-        if not line or line.startswith('~~'):
-            continue
-        if not line.startswith('https:'):
-            line = line.split('|')
-            ch_name = line[0].strip()
-            grp_title = line[1].strip().title()
-            tvg_logo = line[2].strip()
-            tvg_id = line[3].strip()
+
+def get_specific_line_online(url, line_number):
+    response = s.get(url, headers=headers)
+    if response.status_code == 200:
+        lines = response.text.split('\n')
+        if 1 <= line_number <= len(lines):
+            return lines[line_number - 1]
         else:
-            grab(line)
-            
-if 'temp.txt' in os.listdir():
-    os.system('rm temp.txt')
-    os.system('rm watch*')
+            return None
+    else:
+        return None
+
+chunks = get_specific_line_online(master_url, 3)
+
+prefix = "http://s2.callofliberty.fr/HLS-AES/"
+index = chunks.find(prefix)
+
+if index != -1:
+    shortchunks = chunks[index + len(prefix):]
+else:
+    shortchunks = chunks
+
+print(shortchunks)
