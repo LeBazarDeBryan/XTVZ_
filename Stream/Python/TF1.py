05c717823f35cbbd1e1518741af1504ee508483b
Félx <lebazardebryanoff@gmail.com>
2024-10-22 17:57:28 +0200

    Update
    TF1.py

--- Stream/Python/TF1.py
+++ Stream/Python/TF1.py
@@ -1,27 +1,31 @@
 import os
 import streamlink
+import subprocess
 import sys
 
 username = os.environ.get('TF1_USER')
 password = os.environ.get('TF1_PASSWORD')
 
-url = 'https://www.tf1.fr/tf1/direct'
+url = 'https://www.tf1.fr/direct'
 
 session = streamlink.Streamlink()
 
-args = {
-    '--tf1-email': username,
-    '--tf1-password': password,
-}
+try:
+    command = [
+        "streamlink",
+        url,
+        "best",
+        "--username", username,
+        "--password", password,
+        "--hls-live-restart"
+    ]
 
-streams = session.streams(url, **args)
+    result = subprocess.run(command, capture_output=True, text=True)
 
-if streams:
-    stream = streams.get('best')
-    if stream:
-        m3u8_url = stream.to_url()
+    if result.returncode == 0:
+        m3u8_url = result.stdout.strip()
         print(m3u8_url)
     else:
-        print("No 'best' stream available.")
-else:
-    print("No streams found.")
+        print(f"Error: {result.stderr.strip()}")
+except Exception as e:
+    print(f"An error occurred: {e}")
