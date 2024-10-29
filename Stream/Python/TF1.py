93f433cdce7188b0eb6898b10f89060de08bf8aa
Félx <lebazardebryanoff@gmail.com>
2024-10-29 12:33:18 +0100

    Update
    TF1.py

--- Stream/Python/TF1.py
+++ Stream/Python/TF1.py
@@ -3,7 +3,7 @@ import subprocess
 def generate_m3u8(streamlink_url, output_filename="stream.m3u8"):
     try:
         result = subprocess.run(
-            ["streamlink", "--tf1-purge-credentials", "--tf1-email=%email%", "--tf1-password=%password%", streamlink_url, "best", "--stream-url"],
+            ["streamlink", "--tf1-purge-credentials", "--tf1-email=$TF1_USER", "--tf1-password=$TF1_PASSWORD", streamlink_url, "best", "--stream-url"],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True,
