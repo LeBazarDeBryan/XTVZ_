4613ea66b8418ac1825a78da317e829a659b0de1
Félx <lebazardebryanoff@gmail.com>
2024-10-30 13:43:43 +0100

    Update
    TF1.py

--- Stream/Python/TF1.py
+++ Stream/Python/TF1.py
@@ -7,9 +7,6 @@ def generate_m3u8_content(streamlink_url):
         tf1_password = os.environ.get("TF1_PASSWORD")
 
         if not tf1_user or not tf1_password:
-            print("#EXTM3U")
-            print("#EXT-X-VERSION:6")
-            print("#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000")
             print("https://raw.githubusercontent.com/LeBazarDeBryan/XTVZ_/main/Images/Offline.mp4")
             print("\n")
             print("Error: TF1_USER or TF1_PASSWORD environment variable is NOT set.")
@@ -50,18 +47,12 @@ def generate_m3u8_content(streamlink_url):
             )
             return m3u8_content
         else:
-            print("#EXTM3U")
-            print("#EXT-X-VERSION:6")
-            print("#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000")
             print("https://raw.githubusercontent.com/LeBazarDeBryan/XTVZ_/main/Images/Offline.mp4")
             print("\n")
             print("Error: Streamlink: stdout:", result.stdout.strip())
             return None
 
     except Exception as e:
-        print("#EXTM3U")
-        print("#EXT-X-VERSION:6")
-        print("#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000")
         print("https://raw.githubusercontent.com/LeBazarDeBryan/XTVZ_/main/Images/Offline.mp4")
         print("\n")
         print(f"Error: {e}")
