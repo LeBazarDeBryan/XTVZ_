4772ac61cf7d9af8c6052d6643dabe5fe400be3b
Félx <lebazardebryanoff@gmail.com>
2024-12-27 10:15:21 +0000

    Update
    France24.py

--- Stream/Python/France24.py
+++ Stream/Python/France24.py
@@ -14,7 +14,6 @@ def grab(url):
             if windows:
                 print('https://raw.githubusercontent.com/LeBazarDeBryan/XTVZ_/main/Images/Offline.mp4')
                 return
-            #os.system(f'wget {url} -O temp.txt')
             os.system(f'curl "{url}" > temp.txt')
             response = ''.join(open('temp.txt').readlines())
             if '.m3u8' not in response:
@@ -34,7 +33,6 @@ def grab(url):
     hd = streams[-1].strip()
     st = hd.find('http')
     print(hd[st:].strip())
-    #print(f"{link[start : end]}")
 
 print('#EXTM3U')
 print('#EXT-X-VERSION:3')
